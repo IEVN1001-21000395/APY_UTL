3098c8821f5555d9fb379ac6c2c276fd1d66dd8b
Cristian Santiago Serrano Ferro <cserranoferro171@gmail.com>
2024-10-28 21:11:24 -0600

    Segundo Commit Api

--- src/app.py
+++ src/app.py
@@ -1,33 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+from flask_mysqldb import Mysql
+
+from config import config 
+
+
 app=Flask(__name__)
 
-@app.route("/")
-def index():
-    titulo='IEVN-1001'
-    list=['Pedro,Pablo,Juan,Fulta']
-    return render_template('uno.html',titulo=titulo,list=list)
+con=MySQL(app)
 
+@app.route("/alumnos",methods=['GET'])
+def lista_alumnos():
+    try:
+        cursor=con.connection.curso()
+        sql="select * from alumnos"
+        cursor.execute(sql)
+        datos=cursor.fetchall()
+        alumnos=[]
+        for fila in datos:
+            alumno={"matricula":fila[0],"nombre":fila[1],"apaterno":fila[2],"amaterno":fila[3],"correo":fila[4]}
+            alumnos.append(alumno)
+        return jsonify({'alumnos':alumnos,'mensaje':'Lista de alumnos','exito':True})
+    except Exception as ex:
+        return jsonify({"message":"error {}".format(ex),'exito':False})
+    
+    if __name__=="__main__":
+        app.config.from_object(config['development'])
+        app.register_error_hander(404,pagina_no_encontrada)
+        app.run(host='0.0.0.0',port=500)
 
-@app.route("/user/<string:user>") #rutas 
-def user(user):
-    return "El usuario es: {}".format(user)
-
-@app.route("/numero/<int:n1>") #rutas 
-def numero(n1):
-    return "El numero es: {}".format(n1)
-
-@app.route("/user/<string:nom>/int:id") #rutas 
-def datos(nom,id):
-    return "El numero es: {}".format(id,nom)
-
-@app.route("/suma/<float:n1>/float:n2") #rutas 
-def suma(n1,n2):
-    return "La suma es : {}".format(n2+n1)
-
-@app.route("/default")
-@app.route("/default/<string:nom>") #rutas 
-def nom2(nom='Santiago'):
-    return "<h1> El nombre es : {}</h1>".format(nom)#definimos 
-
-if __name__=="__main__":
-    app.run(debug=True)
